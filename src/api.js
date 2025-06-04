575ca6faf4bb7a360b174faab7afe034823a63ab
Your Name <you@example.com>
2025-06-04 13:17:54 +0530

    last commit

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://127.0.0.1:8000/";
+export const BASE_URL = "https://ecommerse-shoppit.onrender.com";
 
 const api = axios.create({
     baseURL: BASE_URL,
